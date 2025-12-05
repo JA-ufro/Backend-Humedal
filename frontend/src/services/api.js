29b92a2223bbb7e24b281a9e1f50a000e5b62427
JA-ufro <j.aravena08@ufromail.cl>
2025-12-04 23:46:01 -0300

    Actualizar localhost a ip servidor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: 'http://200.13.5.6:8080/api', 
   headers: {
     'Content-Type': 'application/json',
   },
